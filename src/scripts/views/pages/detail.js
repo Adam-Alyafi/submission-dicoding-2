7f27e7e74d0452a778f53a48be5c84909b35d250
Adam-Alyafi1998 <adam.muhammad1998@gmail.com>
2021-06-12 15:42:10 +0700

    submission 2

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,27 @@
+import UrlParser from '../../routes/url-parser';
+import TheRestaurantDbSource from '../../data/therestaurantdb-source';
+import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
+
+const Detail = {
+  async render() {
+    return `
+      <div id="restaurant" class="restaurant"></div>
+      <div id="likeButtonContainer"></div>
+    `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await TheRestaurantDbSource.detailRestaurant(url.id);
+    const restaurantsContainer = document.querySelector('#restaurant');
+    restaurantsContainer.innerHTML += createRestaurantDetailTemplate(restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant,
+    });
+  },
+};
+
+export default Detail;
